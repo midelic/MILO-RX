4481ef5452302eb859e16757c8c584203246b254
midelic <midelic@gmail.com>
2022-08-04 10:11:16 +0100

    cleaning

--- pins.h
+++ pins.h
@@ -2,27 +2,26 @@
 //GPIO 6 to GPIO11 are conected to SPI flash and not recommended to use
 //***********************************************************
 	//SX1280
-	#define BIND_pin               0//GPIO 0 -Flashing  pin
-	#define LED_pin                 16
-	#define	SX1280_RST_pin    2		
+	#define BIND_pin         0 //GPIO 0 ;Flashing  pin
+	#define LED_pin          16
+	#define	SX1280_RST_pin   2		
 	#define	SX1280_BUSY_pin  5
-	#define	SX1280_DIO1_pin  4
-	
+	#define	SX1280_DIO1_pin  4	
 	//SPI	
-	#define SX1280_MOSI_pin   13
-	#define SX1280_MISO         12
-	#define SX1280_SCK           14
+	#define SX1280_MOSI_pin    13
+	#define SX1280_MISO        12
+	#define SX1280_SCK         14
 	#define SX1280_CSN_pin     15
-	//
+	//Serial
 	#define SX1280_SPORT_RX_pin 3 // RX SPORT
 	#define SX1280_SPORT_TX_pin 3 // TX SPORT
 	#define SX1280_SBUS_TX_pin   1//SBUS 
-	
-	//#ifndef EL24P
-	//#define ANT_SEL_pin                    9 
-		//Frontend PA/LNA
-	//#define SX1280_TXEN_pin        10
-	//#endif
+	//Frontend PA/LNA	
+        #ifndef EL24P
+        #define ANT_SEL_pin             9 
+	#define SX1280_TXEN_pin        10
+        #define POWER_OUTPUT_FIXED      3
+	#endif
 	
 	#define BIND_SET_INPUT		pinMode(BIND_pin,INPUT)
 	#define BIND_SET_PULLUP		digitalWrite(BIND_pin,HIGH)	
@@ -34,8 +33,6 @@
 	#define	IS_SX1280_DIO1_off		( digitalRead(SX1280_DIO1_pin)==LOW )
 	#define	IS_SX1280_BUSY_on		( digitalRead(SX1280_BUSY_pin)==HIGH )
 	#define	IS_SX1280_BUSY_off		( digitalRead(SX1280_BUSY_pin)==LOW)
-	#define	IS_SX1280_FAN_off        ( digitalRead(SX1280_FAN_EN_pin)==LOW)
-	#define	IS_SX1280_FAN_on        ( digitalRead(SX1280_FAN_EN_pin)==HIGH )
 	#define	IS_LED_on		                ( digitalRead(LED_pin)==HIGH)
 	
 	#define	SX1280_RST_on	           digitalWrite(SX1280_RST_pin,HIGH)
@@ -47,8 +44,6 @@
 		
 	#define SX1280_CSN_on            digitalWrite(SX1280_CSN_pin,HIGH)
 	#define SX1280_CSN_off            digitalWrite(SX1280_CSN_pin,LOW)
-	#define SX1280_FAN_EN_on       digitalWrite(SX1280_FAN_EN_pin,HIGH)
-	#define SX1280_FAN_EN_off       digitalWrite(SX1280_FAN_EN_pin,HIGH)
 	
 	#define	LED_on			               digitalWrite(LED_pin,HIGH)
 	#define	LED_off			               digitalWrite(LED_pin,LOW)
@@ -56,7 +51,6 @@
 	#define	LED_output		            pinMode(LED_pin,OUTPUT)
 	
 	#define USE_SX1280_DCDC
-	#define Regulatory_Domain_ISM_2400 1
 	
 	#define  cli() 			noInterrupts()
 	#define  sei() 			interrupts()
