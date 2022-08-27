5ad30cbf68d8e8536607c39037ddbfd44a0773bc
diego <diego0815@github.com>
2022-08-27 19:36:59 +0200

    FHSS/complement2 port over from TX

--- MiLo_RX/FHSS.h
+++ MiLo_RX/FHSS.h
@@ -1,96 +1,102 @@
-//#pragma once
+#pragma once
 //from mLRS
 //https://github.com/olliw42/mLRS/
 // 2406.0 ... 2473.0  in 1 MHz steps
 // = 68 channels//BW is 812Khz so band resolution of 1Mhz is suitable and no adiacent chanels so they will be randomized
-#define SX1280_FREQ_XTAL_HZ               52000000
-#define SX1280_FREQ_GHZ_TO_REG(f_ghz)     (uint32_t)((double)f_ghz*1.0E9*(double)(1 << 18)/(double)SX1280_FREQ_XTAL_HZ)
-#define SX1280_REG_FIRMWARE_VERSION_MSB   0x0153 // address of the register holding firmware version MSB
-#define SX1280_POWER_DBM_TO_REG(dbm)      (uint8_t)( (int8_t)dbm + 18 ) // -18 dbm = power 0, 13 dbm = power 31
+#define SX1280_FREQ_XTAL_MHZ               52
+#define SX1280_FREQ_GHZ_TO_REG(f_mhz)     (uint32_t)(((double)f_mhz/(double)SX1280_FREQ_XTAL_MHZ)*(double)(1 << 18))
 // this is what is suggested by Semtech
 // #include <math.h>
 // #define SX1280_FREQ_STEP                  ((double)(SX1280_FREQ_XTAL_HZ / pow(2.0,18.0))) // 198.3642578
 // #define SX1280_FREQ_HZ_TO_REG(f_hz)       ((uint32_t)( (double)f_hz / (double)SX1280_FREQ_STEP ))
 // the pow(x,y) is ugly, and using GHz units is more convenient
 
-const uint32_t fhss_freq_list_2p4[] = {
-    SX1280_FREQ_GHZ_TO_REG(2.406), // channel 0
-    SX1280_FREQ_GHZ_TO_REG(2.407),
-    SX1280_FREQ_GHZ_TO_REG(2.408),
-    SX1280_FREQ_GHZ_TO_REG(2.409),
-    
-    SX1280_FREQ_GHZ_TO_REG(2.410), // channel 4
-    SX1280_FREQ_GHZ_TO_REG(2.411),
-    SX1280_FREQ_GHZ_TO_REG(2.412),
-    SX1280_FREQ_GHZ_TO_REG(2.413),
-    SX1280_FREQ_GHZ_TO_REG(2.414),
-    SX1280_FREQ_GHZ_TO_REG(2.415),
-    SX1280_FREQ_GHZ_TO_REG(2.416),
-    SX1280_FREQ_GHZ_TO_REG(2.417),
-    SX1280_FREQ_GHZ_TO_REG(2.418),
-    SX1280_FREQ_GHZ_TO_REG(2.419),
-    
-    SX1280_FREQ_GHZ_TO_REG(2.420), // channel 14
-    SX1280_FREQ_GHZ_TO_REG(2.421),
-    SX1280_FREQ_GHZ_TO_REG(2.422),
-    SX1280_FREQ_GHZ_TO_REG(2.423),
-    SX1280_FREQ_GHZ_TO_REG(2.424),
-    SX1280_FREQ_GHZ_TO_REG(2.425),
-    SX1280_FREQ_GHZ_TO_REG(2.426),
-    SX1280_FREQ_GHZ_TO_REG(2.427),
-    SX1280_FREQ_GHZ_TO_REG(2.428),
-    SX1280_FREQ_GHZ_TO_REG(2.429),
-    
-    SX1280_FREQ_GHZ_TO_REG(2.430), // channel 24
-    SX1280_FREQ_GHZ_TO_REG(2.431),
-    SX1280_FREQ_GHZ_TO_REG(2.432),
-    SX1280_FREQ_GHZ_TO_REG(2.433),
-    SX1280_FREQ_GHZ_TO_REG(2.434),
-    SX1280_FREQ_GHZ_TO_REG(2.435),
-    SX1280_FREQ_GHZ_TO_REG(2.436),
-    SX1280_FREQ_GHZ_TO_REG(2.437),
-    SX1280_FREQ_GHZ_TO_REG(2.438),
-    SX1280_FREQ_GHZ_TO_REG(2.439),
-    
-    SX1280_FREQ_GHZ_TO_REG(2.440), // channel 34
-    SX1280_FREQ_GHZ_TO_REG(2.441),
-    SX1280_FREQ_GHZ_TO_REG(2.442),
-    SX1280_FREQ_GHZ_TO_REG(2.443),
-    SX1280_FREQ_GHZ_TO_REG(2.444),
-    SX1280_FREQ_GHZ_TO_REG(2.445),
-    SX1280_FREQ_GHZ_TO_REG(2.446),
-    SX1280_FREQ_GHZ_TO_REG(2.447),
-    SX1280_FREQ_GHZ_TO_REG(2.448),
-    SX1280_FREQ_GHZ_TO_REG(2.449),
-    
-    SX1280_FREQ_GHZ_TO_REG(2.450), // channel 44
-    SX1280_FREQ_GHZ_TO_REG(2.451),
-    SX1280_FREQ_GHZ_TO_REG(2.452),
-    SX1280_FREQ_GHZ_TO_REG(2.453),
-    SX1280_FREQ_GHZ_TO_REG(2.454),
-    SX1280_FREQ_GHZ_TO_REG(2.455),
-    SX1280_FREQ_GHZ_TO_REG(2.456),
-    SX1280_FREQ_GHZ_TO_REG(2.457),
-    SX1280_FREQ_GHZ_TO_REG(2.458),
-    SX1280_FREQ_GHZ_TO_REG(2.459),
-    
-    SX1280_FREQ_GHZ_TO_REG(2.460), // channel 54
-    SX1280_FREQ_GHZ_TO_REG(2.461),
-    SX1280_FREQ_GHZ_TO_REG(2.462),
-    SX1280_FREQ_GHZ_TO_REG(2.463),
-    SX1280_FREQ_GHZ_TO_REG(2.464),
-    SX1280_FREQ_GHZ_TO_REG(2.465),
-    SX1280_FREQ_GHZ_TO_REG(2.466),
-    SX1280_FREQ_GHZ_TO_REG(2.467),
-    SX1280_FREQ_GHZ_TO_REG(2.468),
-    SX1280_FREQ_GHZ_TO_REG(2.469),
-    
-    SX1280_FREQ_GHZ_TO_REG(2.470), // channel 64
-    SX1280_FREQ_GHZ_TO_REG(2.471),
-    SX1280_FREQ_GHZ_TO_REG(2.472),
-    SX1280_FREQ_GHZ_TO_REG(2.473), // channel 67
+// channel list expanded to match list in Bluetooth core specification 5.3 page 389: f=2402+k MHz, k=0..78
+const uint32_t fhss_freq_list_2p4[] = 
+{
+    SX1280_FREQ_GHZ_TO_REG(2402+0),  // channel 0, 2402 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+1),  // channel 1, 2403 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+2),  // channel 2, 2404 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+3),  // channel 3, 2405 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+4),  // channel 4, 2406 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+5),  // channel 5, 2407 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+6),  // channel 6, 2408 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+7),  // channel 7, 2409 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+8),  // channel 8, 2410 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+9),  // channel 9, 2411 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+10),  // channel 10, 2412 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+11),  // channel 11, 2413 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+12),  // channel 12, 2414 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+13),  // channel 13, 2415 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+14),  // channel 14, 2416 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+15),  // channel 15, 2417 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+16),  // channel 16, 2418 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+17),  // channel 17, 2419 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+18),  // channel 18, 2420 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+19),  // channel 19, 2421 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+20),  // channel 20, 2422 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+21),  // channel 21, 2423 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+22),  // channel 22, 2424 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+23),  // channel 23, 2425 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+24),  // channel 24, 2426 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+25),  // channel 25, 2427 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+26),  // channel 26, 2428 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+27),  // channel 27, 2429 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+28),  // channel 28, 2430 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+29),  // channel 29, 2431 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+30),  // channel 30, 2432 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+31),  // channel 31, 2433 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+32),  // channel 32, 2434 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+33),  // channel 33, 2435 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+34),  // channel 34, 2436 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+35),  // channel 35, 2437 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+36),  // channel 36, 2438 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+37),  // channel 37, 2439 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+38),  // channel 38, 2440 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+39),  // channel 39, 2441 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+40),  // channel 40, 2442 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+41),  // channel 41, 2443 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+42),  // channel 42, 2444 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+43),  // channel 43, 2445 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+44),  // channel 44, 2446 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+45),  // channel 45, 2447 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+46),  // channel 46, 2448 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+47),  // channel 47, 2449 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+48),  // channel 48, 2450 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+49),  // channel 49, 2451 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+50),  // channel 50, 2452 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+51),  // channel 51, 2453 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+52),  // channel 52, 2454 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+53),  // channel 53, 2455 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+54),  // channel 54, 2456 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+55),  // channel 55, 2457 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+56),  // channel 56, 2458 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+57),  // channel 57, 2459 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+58),  // channel 58, 2460 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+59),  // channel 59, 2461 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+60),  // channel 60, 2462 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+61),  // channel 61, 2463 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+62),  // channel 62, 2464 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+63),  // channel 63, 2465 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+64),  // channel 64, 2466 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+65),  // channel 65, 2467 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+66),  // channel 66, 2468 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+67),  // channel 67, 2469 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+68),  // channel 68, 2470 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+69),  // channel 69, 2471 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+70),  // channel 70, 2472 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+71),  // channel 71, 2473 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+72),  // channel 72, 2474 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+73),  // channel 73, 2475 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+74),  // channel 74, 2476 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+75),  // channel 75, 2477 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+76),  // channel 76, 2478 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+77),  // channel 77, 2479 MHz
+    SX1280_FREQ_GHZ_TO_REG(2402+78),  // channel 78, 2480 MHz
 };
-const uint8_t fhss_bind_channel_list_2p4[] = {
+
+const uint8_t fhss_bind_channel_list_2p4[] = 
+{
     46 //,14, 33, 61 // just pick some
 };
 
@@ -106,9 +112,10 @@ const uint8_t fhss_bind_channel_list_2p4[] = {
 // redpine:
 // same prng as spektrum, picks 50 channels, ensures not close and not 0,1
 
-#define FHSS_FREQ_LIST_MAX_LEN  80 // 2.4 GHz is 68
-#define FHSS_MAX_NUM    68
-#define FHSS_CHANNELS_NUM  47
+#define FHSS_FREQ_LIST_MAX_LEN  80 
+#define FHSS_MAX_NUM    68  // must be <= FHSS_FREQ_LIST_MAX_LEN
+#define FHSS_CHANNELS_NUM  47  // must be <= FHSS_MAX_NUM
+
 uint32_t _seed;
 bool is_in_binding;
 uint8_t cnt ;
@@ -118,13 +125,14 @@ uint8_t ch_list[FHSS_MAX_NUM]; // that's our list of randomly selected channels
 uint32_t fhss_list[FHSS_MAX_NUM]; // that's our list of randomly selected frequencies
 int8_t fhss_last_rssi[FHSS_MAX_NUM];
 
-uint8_t FREQ_LIST_LEN;
-uint8_t FHSS_BIND_CHANNEL_LIST_LEN;
+uint8_t freq_list_len;
+uint8_t fhss_bind_channel_list_len;
 const uint32_t* fhss_freq_list;
 const uint8_t* fhss_bind_channel_list;
 
 #ifdef USE_HC_FHSS
-    uint32_t fhss_list_hc[47] = {
+    uint32_t fhss_list_hc[47] = 
+    {
         12345974,
         12421592,
         12174572,
@@ -180,8 +188,8 @@ void ICACHE_RAM_ATTR Fhss_Init()
 {
     fhss_freq_list = fhss_freq_list_2p4;
     fhss_bind_channel_list = fhss_bind_channel_list_2p4;
-    FREQ_LIST_LEN = (uint8_t)(sizeof(fhss_freq_list_2p4) / sizeof(uint32_t));
-    FHSS_BIND_CHANNEL_LIST_LEN = (uint8_t)(sizeof(fhss_bind_channel_list_2p4) / sizeof(uint8_t));
+    freq_list_len = (uint8_t)(sizeof(fhss_freq_list_2p4) / sizeof(uint32_t));
+    fhss_bind_channel_list_len = (uint8_t)(sizeof(fhss_bind_channel_list_2p4) / sizeof(uint8_t));
     cnt = FHSS_CHANNELS_NUM;
     curr_i = 0;
     is_in_binding = false;
@@ -203,34 +211,37 @@ void ICACHE_RAM_ATTR Fhss_generate(uint32_t seed)//
 {
     _seed = seed;
     
-    bool used_flag[FREQ_LIST_LEN];
+    bool used_flag[freq_list_len];
     
-    for (uint8_t ch = 0; ch < FREQ_LIST_LEN; ch++) 
+    for (uint8_t ch = 0; ch < freq_list_len; ch++) 
         used_flag[ch] = false;
     
     uint8_t k = 0;
     while (k < cnt) 
     {
-        uint8_t rn = prng() % (FREQ_LIST_LEN - k); // get a random number in the remaining range
+        uint8_t rn = prng() % (freq_list_len - k); // get a random number in the remaining range
         
         uint8_t i = 0;
         uint8_t ch;
         
-        for (ch = 0; ch < FREQ_LIST_LEN; ch++) 
+        for (ch = 0; ch < freq_list_len; ch++) 
         {
-            if (used_flag[ch]) continue;
-            if (i == rn) break; // ch is our next index
+            if (used_flag[ch]) 
+                continue;
+            if (i == rn) 
+                break; // ch is our next index
             i++;
         }
         
-        if (ch >= FREQ_LIST_LEN) 
+        if (ch >= freq_list_len) 
         { // argh, must not happen !
             ch = 0;
         }
         
         // do not pick a bind channel
         bool is_bind_channel = false;
-        for (uint8_t bi = 0; bi < FHSS_BIND_CHANNEL_LIST_LEN; bi++) {
+        for (uint8_t bi = 0; bi < fhss_bind_channel_list_len; bi++) 
+        {
             if (ch == fhss_bind_channel_list[bi])
                 is_bind_channel = true;
         }
@@ -261,7 +272,7 @@ void ICACHE_RAM_ATTR Fhss_generate(uint32_t seed)//
         
         used_flag[ch] = true;
         
-    k++;
+        k++;
     }
     
     curr_i = 0;
@@ -291,10 +302,10 @@ uint32_t ICACHE_RAM_ATTR GetInitialFreq()
     
 uint32_t  ICACHE_RAM_ATTR GetCurrFreq(void)
 {
-if (is_in_binding) 
-    return fhss_freq_list[fhss_bind_channel_list[0]];
+    if (is_in_binding) 
+        return fhss_freq_list[fhss_bind_channel_list[0]];
 
-return fhss_list[curr_i];
+    return fhss_list[curr_i];
 }
     
 uint32_t ICACHE_RAM_ATTR bestX(void)
--- MiLo_RX/SX1280_SPI.ino
+++ MiLo_RX/SX1280_SPI.ino
@@ -487,12 +487,12 @@ void ICACHE_RAM_ATTR BusyDelay(uint32_t duration)
     }
 }
 
-int32_t ICACHE_RAM_ATTR SX1280_complement2( const uint32_t num, const uint8_t bitCnt )
+int32_t ICACHE_RAM_ATTR SX1280_complement2( const uint32_t num, const uint8_t bitCnt ) // convert bitCnt bitlength number in 2-complement notation to int32_t
 {
-    int32_t retVal = ( int32_t )num;
-    if( num >= 2<<( bitCnt - 2 ) )
+    int32_t retVal = ( int32_t )num; //cast bitCnt length number to int32
+    if( retVal & (1<<(bitCnt-1)) ) // check for sign bit at position bitCnt, if not set return value as is, else...
     {
-        retVal -= 2<<( bitCnt - 1 );
+        retVal -= 1<<bitCnt;       // convert from bitCnt 2-complement to 32bit 2-complement format by inverting all bits and subtracting 1, done by subtrating binary value of 1 followed by bitCnt zeroes
     }
     return retVal;
 }
@@ -597,7 +597,7 @@ bool  ICACHE_RAM_ATTR SX1280_GetFrequencyErrorbool()
 void  ICACHE_RAM_ATTR getRFlinkInfo()
 {
     int32_t rssiDBM = LastPacketRSSI;
-    if (antenna = 0){
+    if (antenna == 0){
         if (rssiDBM > 0) 
             rssiDBM = 0;
         // BetaFlight/iNav expect positive values for -dBm (e.g. -80dBm -> sent as 80)
@@ -794,17 +794,6 @@ int32_t ICACHE_RAM_ATTR SX1280_GetLoRaBandwidth( )
     return bwValue;
 }
 
-int32_t ICACHE_RAM_ATTR complement2( const uint32_t num, const uint8_t bitCnt )
-{
-    int32_t retVal = ( int32_t )num;
-    if( num >= 2<<( bitCnt - 2 ) )
-    {
-        retVal -= 2<<( bitCnt - 1 );
-    }
-    return retVal;
-}
-
-
 double ICACHE_RAM_ATTR SX1280_GetFrequencyError( )
 {
     uint8_t efeRaw[3] = {0};
@@ -816,6 +805,6 @@ double ICACHE_RAM_ATTR SX1280_GetFrequencyError( )
     efeRaw[2] = SX1280_ReadReg( SX1280_REG_LR_ESTIMATED_FREQUENCY_ERROR_MSB + 2 );
     efe = ( efeRaw[0]<<16 ) | ( efeRaw[1]<<8 ) | efeRaw[2];
     efe &= SX1280_REG_LR_ESTIMATED_FREQUENCY_ERROR_MASK;
-    efeHz = 1.55 * ( double )complement2( efe, 20 ) / ( 1600.0 / ( double ) SX1280_GetLoRaBandwidth( ) * 1000.0 );
+    efeHz = 1.55 * ( double )SX1280_complement2( efe, 20 ) / ( 1600.0 / ( double ) SX1280_GetLoRaBandwidth( ) * 1000.0 );
     return efeHz;
 }
